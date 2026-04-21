7e50d31178647826a5de4c4b72e8145673043cfc
Elshazly97 <163661979+Elshazly97@users.noreply.github.com>
2026-04-21 23:53:20 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-#import plotly.express as px
-#import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime
 import json
 import os
